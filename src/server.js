fb3bcc7604ecb21ee58715e26f2b313622643bcc
jignesh2619 <136894974+jignesh2619@users.noreply.github.com>
2026-04-26 20:48:31 +0530

    Remove temporary database debug output

--- src/server.js
+++ src/server.js
@@ -3,16 +3,4 @@ const env = require("./config/env");
 
 app.listen(env.port, () => {
   console.log(`School Management API running on port ${env.port}`);
-  console.log("Database config", {
-    host: env.db.host,
-    port: env.db.port,
-    user: env.db.user,
-    database: env.db.database,
-    ssl: env.db.ssl,
-    passwordLength: env.db.password.length,
-    passwordPreview:
-      env.db.password.length > 1
-        ? `${env.db.password[0]}...${env.db.password[env.db.password.length - 1]}`
-        : "missing"
-  });
 });
